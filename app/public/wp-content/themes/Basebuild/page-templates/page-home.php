73d9c0982cedbf32d5b44bfc4315acab1af436d5
Ollie <m.oliver.smith@gmail.com>
2024-04-18 17:58:56 +0100

    BED updates

--- app/public/wp-content/themes/Basebuild/page-templates/page-home.php
+++ app/public/wp-content/themes/Basebuild/page-templates/page-home.php
@@ -7,16 +7,21 @@
 get_header(); ?>
 
 <div class="page">
-<?php // get_template_part( 'template-parts/content', get_post_type() ); ?>
+    <?php get_template_part( 'template-parts/featured-event'); ?>
     <section class="section">
         <div class="container">
             <div class="row">
                 <div class="col-sm-12">
-                    <?php the_content(); ?>
+                    <h2><?php echo get_field('tagline'); ?></h2>
                 </div>
             </div>
         </div>
     </section>
+    <?php get_template_part( 'template-parts/carousel', null, array('type' => 'event_' )); ?>
+    <?php get_template_part( 'template-parts/additional-links', null, array('type' => 'en_' )); ?>
+    <?php get_template_part( 'template-parts/carousel', null, array('type' => 'news_' )); ?>
+    <?php get_template_part( 'template-parts/additional-links', null, array('type' => 'ed_' )); ?>
+
 </div>
 <?php get_footer();
 
