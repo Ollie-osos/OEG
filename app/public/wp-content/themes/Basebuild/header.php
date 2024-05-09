c76074fa4aed409e4a5a3987a01d8fe10e85b359
Ollie <m.oliver.smith@gmail.com>
2024-05-09 18:00:26 +0100

    header and footer updates

--- app/public/wp-content/themes/Basebuild/header.php
+++ app/public/wp-content/themes/Basebuild/header.php
@@ -13,6 +13,23 @@
 // }else{
 //   $page_colour = 'page-colour_pink';
 // }
+$current_day = strtolower(date('l')); // Get the lowercase name of the current day, e.g., 'monday'
+
+// Get the opening time for the current day from ACF
+$opening_time = get_field($current_day, 'option');
+
+if ($opening_time) {
+  $open_today = $opening_time['time'];
+  $entry_fee = $opening_time['entry'];
+} else {
+  $open_today = "Closed";
+  $entry_fee = "100$";
+}
+
+$youtube_url = get_field('youtube', 'option');
+$instagram_url = get_field('instagram', 'option');
+$twitter_url = get_field('twitter', 'option');
+$facebook_url = get_field('facebook', 'option');
 
 ?>
 <!doctype html>
@@ -26,18 +43,41 @@
   <?php wp_head(); ?>
 </head>
 
-<body <?php //body_class($page_colour); ?>>
+<body <?php //body_class($page_colour); 
+      ?>>
 
 
-  <header class="header header-fixed u-unselectable <?php //echo $page_colour; ?>">
-    <div class="header-top">
-      <div class="header-brand">
-        <div class="nav-item no-hover"><a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/dist/img/logo.svg" alt="logo" /></a></div>
-        <div class="nav-item nav-btn" id="header-btn"><span></span><span></span><span></span></div>
-      </div>
-      <div class="header-nav" id="header-menu">
-        <?php foundationpress_main_nav(); ?>
-        <?php get_search_form(); ?>
+  <header class="header u-unselectable">
+    <div class="container">
+      <div class="header-top">
+        <div class="header-brand">
+          <div class="nav-item no-hover"><a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/dist/img/logo-black.svg" alt="logo" /></a></div>
+          <div class="nav-item blue-text uppercase">Opening today:<br><?php echo $open_today; ?><br><?php echo $entry_fee; ?></div>
+          <div class="right-links pt-5">
+            <div class="u-flex">
+              <div class="nav-item top-links">
+                <div class="top-link"><a class="btn btn-primary" href="/coaching">Whats On</a></div>
+                <div class="top-link"><a class="btn btn-primary" href="/courses">Plan your Visit</a></div>
+                <div class="top-link"><a class="btn btn-primary" href="/podcasts">Donate</a></div>
+              </div>
+              <div class="nav-item nav-btn" id="header-btn"><a class="btn btn-blue">MENU</a></div>
+            </div>
+            <div class="u-flex u-justify-flex-end pt-5">
+              <img src="<?php echo get_template_directory_uri(); ?>/dist/img/accessibility.svg" alt="logo" />
+              <img src="<?php echo get_template_directory_uri(); ?>/dist/img/search-icon.svg" alt="logo" />
+            </div>
+            
+          </div>
+        </div>
+        <div class="header-nav header-bottom" id="header-menu">
+          <?php foundationpress_main_nav(); ?>
+          <div class="social-container">
+            <?php if($facebook_url){ echo '<a aria-label="facebook link" target="_blank" href="'.$facebook_url.'"><span class="icon"><i class="fab fa-facebook"></i></span></a>'; } ?>
+            <?php if($twitter_url){ echo '<a aria-label="twitter link" target="_blank" href="'.$twitter_url.'"><span class="icon"><i class="fa-brands fa-x-twitter"></i></span></a>'; } ?>
+            <?php if($instagram_url){ echo '<a aria-label="instagram link" target="_blank" href="'.$instagram_url.'"><span class="icon"><i class="fab fa-instagram"></i></span></a>'; } ?>
+            <?php if($youtube_url){ echo '<a aria-label="youtube link" target="_blank" href="'.$youtube_url.'"><span class="icon"><i class="fab fa-youtube"></i></span></a>'; } ?>
+          </div>
+        </div>
       </div>
     </div>
   </header>
