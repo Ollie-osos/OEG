080285de5bf7ff86027f147d4235f71008ba91c0
Ollie <m.oliver.smith@gmail.com>
2024-05-24 16:24:16 +0100

    access page finish

--- app/public/wp-content/themes/Basebuild/page-templates/page-access.php
+++ app/public/wp-content/themes/Basebuild/page-templates/page-access.php
@@ -11,7 +11,7 @@ get_header(); ?>
         <div class="container">
             <div class="row">
                 <div class="col-sm-12 double-column">
-                    <p><?php the_content(); ?></p>
+                    <?php the_content(); ?>
                 </div>
             </div>
         </div>
@@ -19,23 +19,22 @@ get_header(); ?>
 <section class="section border-bottom">
     <div class="container">
         <div class="row">
-            <div class="col-sm-12 double-column">
-                <?php if (have_rows('access_options')) : 
-                    while (have_rows('access_options')) : the_row(); 
-                        $access_icon = get_sub_field('access_icon'); 
-                        $access_text = get_sub_field('access_text'); ?>
-                        <div class="access_options" >
-                            <img src="<?php echo $access_icon; ?>" alt="access icon">
-                            <p><?php echo $access_text; ?></p>
-                        </div>
-                    <?php endwhile;
-                endif; ?>
-            </div>
+            <?php if (have_rows('access_options')) : 
+                while (have_rows('access_options')) : the_row(); 
+                    $access_icon = get_sub_field('access_icon'); 
+                    $access_text = get_sub_field('access_text'); ?>
+                    <div class="access_options col-md-6 col-sm-12 u-flex" >
+                        <div class="img"><img src="<?php echo $access_icon; ?>" alt="access icon"></div>
+                        <div class="text"><?php echo $access_text; ?></div>
+                    </div>
+                <?php endwhile;
+            endif; ?>
         </div>
     </div>
 </section>
 
 <?php get_template_part( 'template-parts/additional-links', null, array('type' => 'pv_' )); ?>
+<?php get_template_part( 'template-parts/mailing-list'); ?>
 
 </div>
 <?php get_footer();
